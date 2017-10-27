c9ea56a09477eed6c9d948b9c828938531400d3b
James Lovejoy <jameslovejoy1@gmail.com>
2017-10-28 01:31:40 +0200

    Expand allowed versions list

--- scanner.js
+++ scanner.js
@@ -245,7 +245,9 @@ function Scanner(options) {
                                "b092f6b", 
                                "b092f6b-dirty",
 			       "649807a",
-			       "649807a-dirty"];
+			       "649807a-dirty"
+                               "d7ddaa4",
+                               "d7ddaa4-dirty"];
 
         digest_ip(info, function(err, fee){
             if(!err) {
